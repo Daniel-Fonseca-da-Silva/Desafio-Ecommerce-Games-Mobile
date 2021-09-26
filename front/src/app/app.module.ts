852ec939593545bbc88f4561ded6c151e1754289
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-09-26 14:46:09 -0300

    Criado navbar

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ToolbarComponent } from './component/toolbar/toolbar.component';
+import { MatIconModule } from '@angular/material/icon';  
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
