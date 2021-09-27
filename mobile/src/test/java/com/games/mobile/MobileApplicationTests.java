390b8a1bc2ab40580541c8f3fa54b4eb37a5aa10
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-09-27 15:24:56 -0300

    Projeto back criado com spring initializr

---
+++ mobile/src/test/java/com/games/mobile/MobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.games.mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
