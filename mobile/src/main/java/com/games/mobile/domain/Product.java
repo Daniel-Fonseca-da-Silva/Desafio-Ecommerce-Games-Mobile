e035a745428b5a3399081aadfbac2dd64fbc95d6
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-09-27 15:41:24 -0300

    Adicionado Project, dependências e configurado H2

---
+++ mobile/src/main/java/com/games/mobile/domain/Product.java
@@ -0,0 +1,90 @@
+package com.games.mobile.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Product implements Serializable {
+
+    private static final long serialVersionUID;
+
+    static {
+        serialVersionUID = 1L;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Double price;
+    private Integer score;
+    private String image;
+
+    public Product() {
+    }
+
+    public Product(Integer id, String name, Double price, Integer score, String image) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.score = score;
+        this.image = image;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
