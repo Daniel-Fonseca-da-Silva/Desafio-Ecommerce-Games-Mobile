9b0d2219f16c2658185cfb079cd70b2467b75ef6
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-09-27 15:46:16 -0300

    Criado repository de product

---
+++ mobile/src/main/java/com/games/mobile/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.games.mobile.repositories;
+
+import com.games.mobile.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+
+
+}
