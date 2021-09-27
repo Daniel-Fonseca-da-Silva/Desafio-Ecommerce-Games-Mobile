5754b2491d290e3b8ca6c22c58e445fb912e95e0
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-09-27 16:58:00 -0300

    Refatorado algumas partes do projeto

--- mobile/src/main/java/com/games/mobile/MobileApplication.java
+++ mobile/src/main/java/com/games/mobile/MobileApplication.java
@@ -1,13 +1,20 @@
 package com.games.mobile;
 
+import com.games.mobile.domain.Product;
+import com.games.mobile.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MobileApplication {
+public class MobileApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+	}
 }
--- mobile/src/main/java/com/games/mobile/domain/Product.java
+++ mobile/src/main/java/com/games/mobile/domain/Product.java
@@ -10,11 +10,7 @@ import java.util.Objects;
 @Entity
 public class Product implements Serializable {
 
-    private static final long serialVersionUID;
-
-    static {
-        serialVersionUID = 1L;
-    }
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
