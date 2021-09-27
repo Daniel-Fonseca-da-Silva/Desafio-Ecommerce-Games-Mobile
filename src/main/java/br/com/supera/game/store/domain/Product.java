7fce1dcb269defeb5d664ef45be4c71c01269b9b
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-09-27 14:44:34 -0300

    Adicionadas as anotações de Product

--- src/main/java/br/com/supera/game/store/domain/Product.java
+++ src/main/java/br/com/supera/game/store/domain/Product.java
@@ -1,9 +1,23 @@
 package br.com.supera.game.store.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Product {
+@Entity
+public class Product implements Serializable {
 
+    private static final long serialVersionUID;
+
+    static {
+        serialVersionUID = 1L;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private Double price;
