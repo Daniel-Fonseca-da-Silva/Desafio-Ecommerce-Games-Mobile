c519b40bdba73bb6d657948191de565eed0d5373
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-09-27 23:19:17 -0300

    Criado endpoint findAll

--- mobile/src/main/java/com/games/mobile/resources/ProductResource.java
+++ mobile/src/main/java/com/games/mobile/resources/ProductResource.java
@@ -1,5 +1,7 @@
 package com.games.mobile.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,5 +27,10 @@ public class ProductResource {
 
 	}
 
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll() {
+		List<Product> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
 
 }
--- mobile/src/main/java/com/games/mobile/services/ProductService.java
+++ mobile/src/main/java/com/games/mobile/services/ProductService.java
@@ -1,5 +1,6 @@
 package com.games.mobile.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,11 @@ public class ProductService {
 	public Product findById(Integer id) {
 		Optional<Product> obj = repository.findById(id);
 		return obj.orElse(null);
+	}
+
+	public List<Product> findAll() {
+		List<Product> list = repository.findAll();
+		return list;
 	}
 
 }
